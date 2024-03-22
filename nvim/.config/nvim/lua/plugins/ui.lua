f540a96ea4eb3f6ad7d0be75e605c7f7310d15e3
2coolchampion <franvidicek@gmail.com>
2024-03-22 20:49:13 +0100

    feat: add incline plugin
    Make it display buffer filename on buffers in the tab.

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -28,4 +28,25 @@ return {
       },
     },
   },
+
+  -- filename labels
+  {
+    "b0o/incline.nvim",
+    events = "BufReadPre",
+    priority = 1200,
+    config = function()
+      require("incline").setup({
+        window = { margin = { horizontal = 0, vertical = 0 } },
+        hide = { cursorline = true },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+          if vim.bo[props.buf].modified then
+            filename = "[+]" .. filename
+          end
+          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+          return { { icon, guifg = color }, { " " }, { filename } }
+        end,
+      })
+    end,
+  },
 }
