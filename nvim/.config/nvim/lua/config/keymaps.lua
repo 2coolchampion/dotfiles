44c0efd99f5ee16a2eb4c67937f161fc2bdcf75a
2coolchampion <franvidicek@gmail.com>
2024-03-24 09:38:55 +0100

    chore(nvim): add TODO
    Create a keymap that deletes unused buffers

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -23,3 +23,30 @@ keymap.set("n", "<C-w><down>", "<C-w>-")
 
 -- Toggle line wrapping
 keymap.set("n", "<A-z>", ":set wrap!<CR>", opts)
+
+-- TODO: Delete unused buffers
+--
+-- Close hidden buffers
+-- local function close_hidden_buffers()
+--   local open_buffers = {}
+--   -- Collect buffer numbers of all buffers displayed in windows
+--   for _, win in ipairs(vim.api.nvim_list_wins()) do
+--     local buf = vim.api.nvim_win_get_buf(win)
+--     open_buffers[buf] = true
+--   end
+--
+--   -- Close buffers that are not displayed in any window
+--   for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+--     if not open_buffers[buf] and vim.api.nvim_buf_is_loaded(buf) then
+--       vim.api.nvim_buf_delete(buf, { force = true })
+--     end
+--   end
+-- end
+
+-- keymap.set("n", "<leader>bu", close_hidden_buffers, opts)
+--
+-- -- Register keymaps with descriptions using whichkey
+-- local wk = require("which-key")
+-- wk.register({
+--   ["<leader>bu"] = { close_hidden_buffers, "Close Hidden Buffers" },
+-- }, opts)
