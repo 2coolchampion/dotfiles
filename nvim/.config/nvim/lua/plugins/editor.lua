6fa67a4795847c02d7bfd9252668494182811f38
2coolchampion <franvidicek@gmail.com>
2024-04-12 23:06:42 +0200

    feat(nvim): add automatic window resizing

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -100,4 +100,19 @@ return {
       })
     end,
   },
+  { "anuvyklack/middleclass" },
+  { "anuvyklack/animation.nvim" },
+  {
+    "anuvyklack/windows.nvim",
+    requires = {
+      "anuvyklack/middleclass",
+      "anuvyklack/animation.nvim",
+    },
+    config = function()
+      vim.o.winwidth = 10
+      vim.o.winminwidth = 10
+      vim.o.equalalways = false
+      require("windows").setup()
+    end,
+  },
 }
